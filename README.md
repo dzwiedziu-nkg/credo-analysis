fe2d4bc2f7ac25dc8339f212049027af3470755e
nkg <nkg753@gmail.com>
2019-04-18 22:45:01 +0200

    V2 version of server_vector_measure script and renamed from server_vector_ellipse

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ Methods from this project works on data downloaded via
 
 For running you must have `python3` with package manager and virtual environment like `pip` and `virtualenv`.
 
+Originally running on Python 3.6.
 
 ### Prepare to run
 
@@ -23,7 +24,7 @@ pip install -r requirements.txt
 Download data from CREDO server via [credo-api-tools](https://github.com/dzwiedziu-nkg/credo-api-tools) 
 and copy or link the `credo-api-tools/data-exporter/credo-data-export` dir to `../source_data`.
 
-## server_vector_export
+## server_vector_export script
 
 This script export cosmic-ray hits from JSON file to PNG file and CSV file with other parameters from JSON file.
 
@@ -37,19 +38,50 @@ This script export cosmic-ray hits from JSON file to PNG file and CSV file with 
 
 #### CSV columns:
 
-- PNG file name,
-- `id`,
-- `user_id`,
-- `device_id`,
-- `timestamp`,
-- `team_id`,
-- `width`,
-- `height`,
-- `x`,
-- `y`,
-- `latitude`,
-- `longitude`,
-- `altitude`,
-- `accuracy`,
-- `provider`,
-- `time_received`.
+- [1] PNG file name,
+- [2] `id`,
+- [3] `user_id`,
+- [4] `device_id`,
+- [5] `timestamp`,
+- [6] `team_id`,
+- [7] `width`,
+- [8] `height`,
+- [9] `x`,
+- [10] `y`,
+- [11] `latitude`,
+- [12] `longitude`,
+- [13] `altitude`,
+- [14] `accuracy`,
+- [15] `provider`,
+- [16] `time_received`.
+
+
+## measure_vector_export script
+
+This script analyse image of cosmic-ray hit and append measured parameters to CSV file.
+
+- Input: `../output_vector_server`
+- Output: `../output_vector_measure`
+
+### Output format:
+
+- PNG with marked area file name format: `{user_id}/{device_id}/{date_time}_{detection_id}-segm.png`
+- CSV file name format: `{user_id}/{device_id}.csv`
+
+#### New CSV columns:
+
+- [17] nth in image
+- [18] ellipticity (0.0-1.0)
+- [19] elongation (0.0-1.0)
+- [20] eccentricity (0.0-1.0)
+- [21] marked area (pix^2)
+- [22] convex area (pix^2)
+- [23] solidity (div marked per convex)
+- [24] orientation (rad)
+- [25] perimeter (pix)
+- [26] major_axis_length (pix)
+- [27] minor_axis_length (pix)
+- [28] average background bright (0-255)
+- [29] brightest pixel (0-255)
+- [30] sum of (bright of pixel minus average background bright)
+- [31] average hit bright
--- measure_vector_export.py
+++ measure_vector_export.py
@@ -14,14 +14,18 @@ from astropy.visualization.mpl_normalize import ImageNormalize
 from photutils import detect_sources, deblend_sources, source_properties
 
 # Config
-INDIR = '../output_vector_server'  # directory with input CSV and PNG files from server_vector_export script
-OUTDIR = '../output_vector_ellipse'  # directory with output CSV and PNG files with segmentation marks
+from skimage.measure import regionprops
 
-# wszystkie pliki PNG w katalogu "png/" z uwzględnieniem podkatalogów
+INDIR = '../output_vector_server'  # directory with input CSV and PNG files from server_vector_export script
+OUTDIR = '../output_vector_measure'  # directory with output CSV and PNG files with segmentation marks
+
+# list all CSV files
 csvs = glob.iglob(INDIR + '/**/*.csv', recursive=True)
 
+# loop for each found CSV
 for csv in csvs:
     with open(csv, 'r') as fcsv:
+        # loop for reach line in CSV
         for line in fcsv:
             values = line.strip().split('\t')
             if len(values) <= 1:
@@ -35,27 +39,49 @@ for csv in csvs:
             csvdir = join(OUTDIR, user_id)
             pngdir = join(OUTDIR, user_id, device_id)
 
-            # wczytanie pliku PNG z konwersją do grayscale
+            # load PNG and convert to grayscale
             img = Image.open(filename).convert('LA')
-            gray, alpha = img.split()  # oddzielenie kanały grayscale od alpha
-            data = np.asarray(gray)  # konwersja do tablicy NumPy
-            data = np.flipud(data)  # lustrzane odbicie tablicy w pionie by była taka sama jak w pliku PNG
+            gray, alpha = img.split()  # split array to grayscale and alpha cannel
+            data = np.asarray(gray)  # convert to NumPy array
+            data = np.flipud(data)  # flip by Y axis (like in screen)
 
-            # próg detekcji ustalany jako 1/8 w drodze od najciemniejszego do najjaśniejszego pixela, dobrane eksperymentalnie
-            # prór można ustalić osobny dla każdego piksela, tutaj wszystkie maja jeden próg
-            threshold = np.ones(data.shape) * ((data.max() - data.min())/8 + data.min())
+            # find darkness pixel
+            brightest = data.max()
+            darkest = brightest
 
-            # parametry dla detect_sources z tutoriala, detect_sources - wyszukuje obiektów gwiazdowych i mgławicowych
+            rows = data.shape[0]
+            cols = data.shape[1]
+            for x in range(0, cols):
+                for y in range(0, rows):
+                    v = data[y, x]
+                    if v:  # skip pixels with 0 bright
+                        darkest = min(darkest, v)
+
+            # set threshold as 1/8 in line segment from darkness pixel to brightness
+            threshold = np.ones(data.shape) * ((brightest - darkest)/8 + darkest)
+
+            # used parametrs from tutorial for detect_sources
             sigma = 3.0 * gaussian_fwhm_to_sigma    # FWHM = 3.
             kernel = Gaussian2DKernel(sigma, x_size=3, y_size=3)
             kernel.normalize()
             segm = detect_sources(data, threshold, npixels=5, filter_kernel=kernel)
 
-            # jeżeli nic nie znaleziono, dalsze przetwarzanie nie ma sensu. Jeżeli nic nie znaleziono to może być zbyt wysoki
-            # próg detekcji
+            # notify when not found any hit
             if segm.nlabels == 0:
-                print("%s: no found" % filename)
+                print("%s: not found" % filename)
                 continue
+
+            # average bright of background
+            sum_background = 0
+            count_background = 0
+            for x in range(0, cols):
+                for y in range(0, rows):
+                    c = segm.data[y, x]
+                    v = data[y, x]
+                    if v and not c:  # skip pixels with 0 bright
+                        sum_background += v
+                        count_background += 1
+            background_bright = sum_background / count_background if count_background else 0
 
             # rozdzielenie znalezionych obiektów jednych od drugich, parametry jak w tutorialu poza npixels=200,
             # który trzeba było zwiększyć bo niepotrzebnie dzielił na kawałki długie hity, dzięki temu nie dzieli i nie odbiło
@@ -74,6 +100,29 @@ for csv in csvs:
             nth = 0
             for obj in cat:
                 nth += 1
+                brightest_obj = 0
+                brigh_obj_sum = 0
+                brightest_obj_sum = 0
+                brightest_obj_count = 0
+
+                for x in range(0, int(obj.xmax.value - obj.xmin.value + 1)):
+                    for y in range(0, int(obj.ymax.value - obj.ymin.value + 1)):
+                        c = obj.data_cutout[y, x]
+                        v = data[int(obj.ymin.value + y), int(obj.xmin.value + x)]
+                        if c:
+                            brightest_obj = max(v, brightest_obj)
+                            brightest_obj_sum += v
+                            brightest_obj_count += 1
+                            brigh_obj_sum += v - background_bright
+
+                brightest_obj_avg = brightest_obj_sum / brightest_obj_count
+
+                try:
+                    another_props = regionprops(obj.data_cutout)[0]
+                except:
+                    print("%s: skip because: %d x %d" % (filename, int(obj.xmax.value - obj.xmin.value + 1), int(obj.ymax.value - obj.ymin.value + 1)))
+                    continue
+
                 # nazwa pliku oryginalnego
                 # fn_out = output_dir2 + '/%.3f-%d-%s' % (obj.ellipticity.value, obj.area.value, fn)
 
@@ -89,14 +138,26 @@ for csv in csvs:
 
                 # wiersz CSV:
                 # eliptyczność, elongacja, ekscentryczność, powierzchnia, [dane z PNG oddzielone "_"], plik wejściowy, plik wyjściowy
+
                 nvalues = values.copy()
                 nvalues.append(str(nth))
                 nvalues.append(str(obj.ellipticity.value))
                 nvalues.append(str(obj.elongation.value))
                 nvalues.append(str(obj.eccentricity.value))
                 nvalues.append(str(obj.area.value))
+                nvalues.append(str(another_props.convex_area))
+                nvalues.append(str(another_props.solidity))
                 nvalues.append(str(obj.orientation.value))
+                nvalues.append(str(obj.perimeter.value))
+                nvalues.append(str(another_props.major_axis_length))
+                nvalues.append(str(another_props.minor_axis_length))
+
+                nvalues.append(str(background_bright))
+                nvalues.append(str(brightest_obj))
+                nvalues.append(str(brigh_obj_sum))
+                nvalues.append(str(brightest_obj_avg))
                 # nvalues.append(str(obj.source_sum.value))
+
 
                 # print('%f; %f; %f; %d; %s; %s; %s;' % (obj.ellipticity.value, obj.elongation.value, obj.eccentricity.value, obj.area.value, srcs, filename, fn_out))
 
